e7a07c3641b590e005435ba9a5f2a79abc3f95d9
jaume vidal <jaumevibu@gmail.com>
2026-09-18 19:39:11 +0200

    initial commit

---
+++ cyberpunk_tools.py
@@ -0,0 +1,21 @@
+import sys
+
+def print_help():
+    print("Usage: python cyberpunk_tools.py [options]")
+    print("Options:")
+    print("  -h, --help: display this help message")
+    print("  -v, --version: display the current version")
+
+# =================================================================
+VERSION = 0.0.1
+# =================================================================
+if __name__ == '__main__':
+    args=sys.argv[1:]
+
+
+
+    print_help()
+
+
+
+
