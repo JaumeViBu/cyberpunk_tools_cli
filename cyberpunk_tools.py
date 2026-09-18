84e6c0e2bc04879cae1a619016c0f82b754c0ee4
jaume vidal <jaumevibu@gmail.com>
2026-09-18 19:51:36 +0200

    create version option trigger: -v --version prints version to screen

--- cyberpunk_tools.py
+++ cyberpunk_tools.py
@@ -6,16 +6,22 @@ def print_help():
     print("  -h, --help: display this help message")
     print("  -v, --version: display the current version")
 
+def print_version():
+    print(f"cyberpunk tools version: {VERSION}")
 # =================================================================
-VERSION = 0.0.1
+VERSION = "0.0.1"
 # =================================================================
 if __name__ == '__main__':
     args=sys.argv[1:]
-
-
+    if len(args) < 1:
+        print_help()
+        exit(1)
+    if args[0] == "-v" or args[0] == "--version":
+        print_version()
+        exit(0)
 
     print_help()
-
+    exit(2)
 
 
 
