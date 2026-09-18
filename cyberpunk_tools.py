c123870f39eef6cbfb1e1feff388d746618e31eb
jaume vidal <jaumevibu@gmail.com>
2026-09-18 20:47:07 +0200

    Applied corrections for a better cli app operation
    exit -> sys.exit
    option checks
      arg[0]==??? -> arg in ????
    print_help
      usage
        [options] -> option

--- cyberpunk_tools.py
+++ cyberpunk_tools.py
@@ -1,7 +1,7 @@
 import sys
 
 def print_help():
-    print("Usage: python cyberpunk_tools.py [options]")
+    print("Usage: python cyberpunk_tools.py option")
     print("Options:")
     print("  -h, --help: display this help message")
     print("  -v, --version: display the current version")
@@ -12,16 +12,21 @@ def print_version():
 VERSION = "0.0.1"
 # =================================================================
 if __name__ == '__main__':
-    args=sys.argv[1:]
-    if len(args) < 1:
+    if not sys.argv or len(sys.argv) < 2:
+        print(f"No options provided...")
         print_help()
-        exit(1)
-    if args[0] == "-v" or args[0] == "--version":
-        print_version()
-        exit(0)
+        sys.exit(1)
 
+    args=sys.argv[1:]
+    arg=args[0]
+
+    if arg in ["-v", "--version"]:
+        print_version()
+        sys.exit(0)
+
+    print(f"Unknown option: {arg}")
     print_help()
-    exit(2)
+    sys.exit(2)
 
 
 
