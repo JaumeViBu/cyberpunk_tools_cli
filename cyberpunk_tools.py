c76e30eb7220872d2e592ef929e1f796858a638d
jaume vidal <jaumevibu@gmail.com>
2026-09-18 21:04:43 +0200

    Move constants to top of file
    sys.argv will always be at least one,
    path given by the so on exec so changed
    the initial arg count check
    
    add check for help option
    
    add message for errors + write those to
    stderr

--- cyberpunk_tools.py
+++ cyberpunk_tools.py
@@ -1,30 +1,39 @@
 import sys
 
+VERSION = "0.0.1"
+
+# =================================================================
+
 def print_help():
-    print("Usage: python cyberpunk_tools.py option")
+    print("Usage:\n  cyberpunk_tools <option>")
     print("Options:")
     print("  -h, --help: display this help message")
     print("  -v, --version: display the current version")
 
 def print_version():
     print(f"cyberpunk tools version: {VERSION}")
+
 # =================================================================
-VERSION = "0.0.1"
-# =================================================================
+
 if __name__ == '__main__':
-    if not sys.argv or len(sys.argv) < 2:
-        print(f"No options provided...")
+
+    args=sys.argv[1:]
+
+    if len(args) < 1:
+        print("No options provided...",file=sys.stderr)
         print_help()
         sys.exit(1)
 
-    args=sys.argv[1:]
     arg=args[0]
 
     if arg in ["-v", "--version"]:
         print_version()
         sys.exit(0)
+    if arg in ["-h", "--help"]:
+        print_help()
+        sys.exit(0)
 
-    print(f"Unknown option: {arg}")
+    print(f"Unknown option: {arg}",file=sys.stderr)
     print_help()
     sys.exit(2)
 
